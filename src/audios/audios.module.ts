321e8ee267053ed0dbf45bf7a1bb98716e5d1713
alikashmar8 <alikashmar8@gmail.com>
2025-06-01 22:29:18 +0300

    Add TypeOrmModule integration for Audio entity in Audios module

--- src/audios/audios.module.ts
+++ src/audios/audios.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AudiosController } from './audios.controller';
 import { AudiosService } from './audios.service';
+import { Audio } from './entities/audio.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Audio])],
   controllers: [AudiosController],
   providers: [AudiosService],
 })
